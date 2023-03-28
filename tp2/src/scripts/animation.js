d8d5765866ebdea03ac2aa9e31fd84b03b6b1fe6
Sara Lyna OUYAHIA <saralyna2002@gmail.com>
2023-03-28 16:00:34 +0200

    debut tp2

--- tp2/src/scripts/animation.js
+++ tp2/src/scripts/animation.js
@@ -2,9 +2,28 @@ import Ball from './ball';
 
 /* TYPE Animation */
 export default class Animation {
+  /* constructor of the class */
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.ball = new Ball(canvas.width/2, canvas.height/2, 20);
+    this.animationRequest = null;
+  }
+
+  animate() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ball.move();
+    this.ball.draw(this.context);
+    this.animationRequest = requestAnimationFrame(this.animate.bind(this));
+  }
 
   /* start the animation or stop it if previously running */
   startAndStop() {
-    // à définir
+    if (this.animationRequest) {
+      cancelAnimationFrame(this.animationRequest);
+      this.animationRequest = null;
+    } else {
+      this.animationRequest = requestAnimationFrame(this.animate.bind(this));
+    }
   }
 }
--- tp2/src/scripts/ball.js
+++ tp2/src/scripts/ball.js
@@ -6,17 +6,35 @@ import ballImgSrc from './assets/images/ball.png';
 export default class Ball {
 
 	static BALL_WIDTH = 48;
+	static BALL_HEIGHT= 48;
 
-  constructor() {
-    // A COMPLETER...
+  constructor(x, y, deltaX = 3, deltaY = -2) {
+		this.x = x;
+    this.y = y;
+    this.deltaX = deltaX;
+    this.deltaY = deltaY;
     this.image = this.#createImage(ballImgSrc);
   }
 
   /* draw this ball, using the given drawing 2d context */
   draw(context) {
-    alert('vous devez coder la méthode draw() de Ball');
+  //  alert('vous devez coder la méthode draw() de Ball');
+		context.drawImage(this.image, this.x, this.y, Ball.BALL_WIDTH, Ball.BALL_HEIGHT);
   }
+  /* la méthode move() */
+	move(canvas) {
+	 let X = this.x + this.deltaX;
+	 let Y = this.y + this.deltaY;
 
+	 if (X < 0 || X + Ball.BALL_WIDTH > canvas.width) {
+		 this.deltaX = -this.deltaX;
+	 }
+	 if (Y < 0 || Y + Ball.BALL_HEIGHT > canvas.height) {
+		 this.deltaY = -this.deltaY;
+	 }
+	 this.x += this.deltaX;
+	 this.y += this.deltaY;
+  }
 
   /* crée l'objet Image à utiliser pour dessiner cette balle */
   #createImage(imageSource) {
