ed2bd470045469d75afb1dcb3dc52f2270ec326c
Sara Lyna OUYAHIA <saralyna2002@gmail.com>
2023-04-19 23:12:43 +0200

    suite

--- tp3/src/scripts/Cible.js
+++ tp3/src/scripts/Cible.js
@@ -11,7 +11,7 @@ export default class Cible extends GameElement{
 
   constructor(x , widthC, heightC) {
     super( cibleSrc,x, 0, 0, 1);
-        this.x = (widthC - this.width));
+        this.x =widthC - this.width;
         this.y = 0;
         this.deltaX=0;
         this.deltaY=1;
